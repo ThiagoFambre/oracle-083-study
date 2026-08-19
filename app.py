6dbf73f0b7d0ee3e9d6785b4c63ddd96af832742
THIAGO MARIN FAMBRE <59978942+ThiagoFambre@users.noreply.github.com>
2026-08-19 13:35:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import fitz
+from rapidfuzz import process
 
 st.set_page_config(
     page_title="Oracle 1Z0-083",
@@ -10,22 +11,58 @@ st.title("📚 Oracle 1Z0-083 Study")
 
 pdf_file = "Exam Dump 1Z0-083.pdf"
 
-try:
+@st.cache_data
+def carregar_pdf():
 
-    documento = fitz.open(pdf_file)
+    doc = fitz.open(pdf_file)
 
-    quantidade_paginas = len(documento)
+    paginas = []
 
-    st.success(
-        f"PDF carregado com sucesso!"
+    for numero, pagina in enumerate(doc):
+
+        texto = pagina.get_text()
+
+        paginas.append({
+            "pagina": numero + 1,
+            "texto": texto
+        })
+
+    return paginas
+
+dados = carregar_pdf()
+
+st.success(
+    f"PDF carregado com {len(dados)} páginas."
+)
+
+texto_busca = st.text_input(
+    "Digite parte da pergunta:"
+)
+
+if texto_busca:
+
+    textos = [
+        p["texto"]
+        for p in dados
+    ]
+
+    resultado = process.extractOne(
+        texto_busca,
+        textos
     )
+
+    texto_encontrado, score, indice = resultado
 
     st.write(
-        f"Total de páginas: {quantidade_paginas}"
+        f"Similaridade: {score:.2f}%"
     )
 
-except Exception as erro:
+    st.subheader(
+        f"Página {dados[indice]['pagina']}"
+    )
 
-    st.error(
-        f"Erro ao abrir PDF: {erro}"
+    st.text_area(
+        "Conteúdo encontrado",
+        texto_encontrado,
+        height=400
     )
